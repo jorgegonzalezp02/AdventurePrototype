384df6ac64da4dd8de10496bbfd0b4fd15957e77
jorgegonzalezp02 <jorjais486@gmail.com>
2023-05-07 04:13:59 -0700

    Last scene changes, new assets, and readme changes

--- README.md
+++ README.md
@@ -1,22 +1,23 @@
 A simple adventure game by {who?} based on a simple adventure game engine by [Adam Smith](https://github.com/rndmcnlly).
 
 Code requirements:
-- **4+ scenes based on `AdventureScene`**: unsatisfied (name at least 4 of the classes).
-- **2+ scenes *not* based on `AdventureScene`**: unsatisfied (name the classes).
+- **4+ scenes based on `AdventureScene`**: Street1, Street2, Street3, Store, and Alley.
+- **2+ scenes *not* based on `AdventureScene`**: Intro, Loading_Screen, and Outro.
 - **2+ methods or other enhancement added to the adventure game engine to simplify my scenes**:
-    - Enhancement 1: unsatisfied (name the method and explain the use of it).
-    - Enhancement 2: unsatisfied (name the method and explain the use of it).
+    - Enhancement 1: The gotoScene method was used when clicking on an arrow and makes a sound before transitioning into the next scene.
+    - Enhancement 2: The showMessage method was used to give descriptions of the characters, objects, and creatures used in each of the scenes.
 
 Experience requirements:
-- **4+ locations in the game world**: unsatisfied (name at least 4 of the classes).
-- **2+ interactive objects in most scenes**: unsatisfied (describe two examples)
-- **Many objects have `pointerover` messages**: unsatisfied (describe two examples)
-- **Many objects have `pointerdown` effects**: unsatisfied (describe two examples)
-- **Some objects are themselves animated**: unsatisfied (describe two examples)
+- **4+ locations in the game world**: The loactions for the game are: First Street, Second Street, Third Street, Store, and Alley Behind the Store.
+- **2+ interactive objects in most scenes**: You can interact with the characters and objects by just clicking them to reveal a funny or useful message and by clicking an item or character, a "ding" sound will be played.
+- **Many objects have `pointerover` messages**: When hovering over the characters or objects, the right hand box will display a message related to the character or object. 
+- **Many objects have `pointerdown` effects**: Upon clicking certain objects, you can obtain items for your inventory that can be useful later in the game. 
+- **Some objects are themselves animated**: Upon defeating the Garr monster, it will disappear and when you pick up the gas tank left behind by the monster it will disappear as well. 
 
 Asset sources:
 - (For each image/audio/video asset used, describe how it was created. What tool did you use to create it? Was it based on another work? If so, how did you change it, and where can we learn more about the original work for comparison? Use [Markdown link syntax](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax#links).)
+**For the sprite and audio assets, the sprites were made in pixilart.com by me Jorge Gonzalez and the audio was made in beepbox.co also made by me. As for the background images, these were made using an AI Art Generator app called StaryAI with prompts "Alley behing a store", "Inside of a store with a door at the end leading to an alley", "Inside of a store", and "Random street".**
 
 Code sources:
-- `adventure.js` and `index.html` were created for this project [Adam Smith](https://github.com/rndmcnlly) and edited by me.
-- `game.js` was sketched by [Adam Smith](https://github.com/rndmcnlly) and rewritten by me.
+- `adventure.js` and `index.html` were created for this project [Adam Smith](https://github.com/rndmcnlly) and edited by me (Jorge Gonzalez).
+- `game.js` was sketched by [Adam Smith](https://github.com/rndmcnlly) and rewritten by me (Jorge Gonzalez).
--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// first street: complete
 class Street1 extends AdventureScene {
     constructor() {
         super("street1", "First Street");
@@ -9,6 +10,8 @@ class Street1 extends AdventureScene {
         this.load.image('street1', "images/street1.png");
         this.load.image('trash_can', "images/trash_can.png");
         this.load.image('arrow', "images/arrow.png");
+        this.load.audio('main', "audio/main_theme.mp3");
+        this.load.audio('ding', "audio/ding_sound.mp3");
     }
 
     onEnter() {
@@ -19,20 +22,18 @@ class Street1 extends AdventureScene {
         street1bg.setDepth(0);
         street1bg.setScale(2.85,2);
 
+        let bgtheme = this.sound.add("main")
+        bgtheme.play();
+        let ding = this.sound.add("ding");
+
         // alfred settings
         let alfred = this.add.image(300, 800,'alfred')
             .setInteractive()
             .on('pointerover', () => this.showMessage("That's Alfred (you), he's pretty chill."))
             .on('pointerdown', () => {
+                ding.play()
                 this.showMessage("Please don't poke him, he'll cry (not really but you get the idea).");
-                this.tweens.add({
-                    targets: clip,
-                    x: '+=' + this.s,
-                    repeat: 2,
-                    yoyo: true,
-                    ease: 'Sine.inOut',
-                    duration: 100
-                });
+                
             });
         alfred.setScale(0.7)
         
@@ -43,6 +44,7 @@ class Street1 extends AdventureScene {
                 this.showMessage("Trash can. What else is it gonna be?")
             })
             .on('pointerdown', () => {
+                ding.play()
                 this.showMessage("You check the trash. Nothing there, now you got your hands dirty. Nasty.");
             })
         trash_can.setScale(0.1)
@@ -54,6 +56,7 @@ class Street1 extends AdventureScene {
             this.showMessage("Trash can. What else is it gonna be?")
         })
         .on('pointerdown', () => {
+            ding.play()
             this.showMessage("You check the trash. Nothing there, now you got your hands dirty. Nasty.");
         })
         trash_can2.setScale(0.3)
@@ -65,15 +68,9 @@ class Street1 extends AdventureScene {
             this.showMessage("Trash can. What else is it gonna be?")
         })
         .on('pointerdown', () => {
+            ding.play()
             this.showMessage("You check the trash. Wait, car keys?! Well, that's convenient.");
             this.gainItem('Car keys');
-            this.tweens.add({
-                targets: t,
-                y: `-=${2 * this.s}`,
-                alpha: { from: 1, to: 0 },
-                duration: 500,
-                onComplete: () => key.destroy()
-            });
         })
         trash_can3.setScale(0.2)
 
@@ -84,6 +81,8 @@ class Street1 extends AdventureScene {
             this.showMessage("To Second Street")
         })
         .on('pointerdown', () => {
+            ding.play()
+            bgtheme.stop()
             this.gotoScene('street2')
         })
         arrow.setScale(0.4)
@@ -94,34 +93,16 @@ class Street1 extends AdventureScene {
             this.showMessage("To Third Street");
         })
         .on('pointerdown', () => {
+            ding.play()
+            bgtheme.stop()
             this.gotoScene('street3');
         })
         arrow2.setScale(0.4)
         arrow2.angle = 45
-
-
-        let door = this.add.text(this.w * 0.1, this.w * 0.15, "🚪 locked door")
-            .setFontSize(this.s * 2)
-            .setInteractive()
-            .on('pointerover', () => {
-                if (this.hasItem("key")) {
-                    this.showMessage("You've got the key for this door.");
-                } else {
-                    this.showMessage("It's locked. Can you find a key?");
-                }
-            })
-            .on('pointerdown', () => {
-                if (this.hasItem("key")) {
-                    this.loseItem("key");
-                    this.showMessage("*squeak*");
-                    door.setText("🚪 unlocked door");
-                    this.gotoScene('demo2');
-                }
-            })
-
     }
 }
 
+// second street: complete
 class Street2 extends AdventureScene {
     constructor() {
         super("street2", "Second Street");
@@ -131,6 +112,8 @@ class Street2 extends AdventureScene {
         this.load.image('alfred', "images/alfred.png");
         this.load.image('street2', "images/street2.png");
         this.load.image('arrow', "images/arrow.png");
+        this.load.audio('main', "audio/main_theme.mp3");
+        this.load.audio('ding', "audio/ding_sound.mp3");
     }
     onEnter() {
 
@@ -139,6 +122,10 @@ class Street2 extends AdventureScene {
         street2bg.setDepth(0);
         street2bg.setScale(2.85,2);
 
+        let bgtheme = this.sound.add('main')
+        bgtheme.play()
+        let ding = this.sound.add('ding')
+
 
         let arrow = this.add.image(150, 500, 'arrow')
         .setInteractive()
@@ -146,6 +133,8 @@ class Street2 extends AdventureScene {
             this.showMessage("To First Street")
         })
         .on('pointerdown', () => {
+            ding.play()
+            bgtheme.stop()
             this.gotoScene('street1')
         })
         arrow.setScale(0.4)
@@ -157,6 +146,8 @@ class Street2 extends AdventureScene {
             this.showMessage("To Third Street");
         })
         .on('pointerdown', () => {
+            ding.play()
+            bgtheme.stop()
             this.gotoScene('street3');
         })
         arrow2.setScale(0.4)
@@ -168,33 +159,20 @@ class Street2 extends AdventureScene {
             this.showMessage("To Store");
         })
         .on('pointerdown', () => {
+            ding.play()
+            bgtheme.stop()
             this.gotoScene('store');
         })
         arrow3.setScale(0.4)
 
-        this.add.text(this.w * 0.3, this.w * 0.4, "just go back")
-            .setFontSize(this.s * 2)
+        let alfred = this.add.image(500, 850,'alfred')
             .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage("You've got no other choice, really.");
-            })
+            .on('pointerover', () => this.showMessage("That's Alfred (you), he's pretty chill."))
             .on('pointerdown', () => {
-                this.gotoScene('street1');
+                ding.play()
+                this.showMessage("Please don't poke him, he'll cry (not really but you get the idea).");
             });
-
-        let finish = this.add.text(this.w * 0.6, this.w * 0.2, '(finish the game)')
-            .setInteractive()
-            .on('pointerover', () => {
-                this.showMessage('*giggles*');
-                this.tweens.add({
-                    targets: finish,
-                    x: this.s + (this.h - 2 * this.s) * Math.random(),
-                    y: this.s + (this.h - 2 * this.s) * Math.random(),
-                    ease: 'Sine.inOut',
-                    duration: 500
-                });
-            })
-            .on('pointerdown', () => this.gotoScene('outro'));
+        alfred.setScale(0.7)
     }
 }
 
@@ -207,6 +185,9 @@ class Street3 extends AdventureScene {
         this.load.image('alfred', "images/alfred.png");
         this.load.image('street3', "images/street3.png");
         this.load.image('arrow', "images/arrow.png");
+        this.load.image('car', "images/car.png");
+        this.load.audio('main', "audio/main_theme.mp3");
+        this.load.audio('ding', "audio/ding_sound.mp3");
     }
     onEnter() {
 
@@ -214,6 +195,270 @@ class Street3 extends AdventureScene {
         street3bg.setOrigin(0);
         street3bg.setDepth(0);
         street3bg.setScale(2.85,2);
+
+        let bgtheme = this.sound.add('main')
+        bgtheme.play()
+        let ding = this.sound.add('ding')
+
+        let arrow = this.add.image(200, 200, 'arrow')
+        .setInteractive()
+        .on('pointerover', () => {
+            this.showMessage("To First Street")
+        })
+        .on('pointerdown', () => {
+            ding.play()
+            bgtheme.stop()
+            this.gotoScene('street1')
+        })
+        arrow.setScale(0.4)
+        arrow.angle = 225
+
+        let arrow2 = this.add.image(1200, 200, 'arrow')
+        .setInteractive()
+        .on('pointerover', () => {
+            this.showMessage("To Second Street");
+        })
+        .on('pointerdown', () => {
+            ding.play()
+            bgtheme.stop()
+            this.gotoScene('street2');
+        })
+        arrow2.setScale(0.4)
+        arrow2.angle = -45
+
+        let car = this.add.image(900, 850, 'car')
+        .setInteractive()
+            .on('pointerover', () => {
+                if (this.hasItem("Car keys")) {
+                    this.showMessage("The keys belong to this car.");
+                } else {
+                    this.showMessage("It's locked. Keys should be on one of the streets.");
+                }
+            })
+            .on('pointerdown', () => {
+                if (this.hasItem("Car keys")) {
+                    if(this.hasItem("Gas tank")) {
+                        this.loseItem("Car keys");
+                        ding.play()
+                        bgtheme.stop()
+                        this.showMessage("It turned on!");
+                        this.gotoScene('outro');
+                    }
+                    else {
+                        ding.play()
+                        this.showMessage("Car is not starting, needs gas. Great.");
+                    }
+                }
+                else {
+                    ding.play()
+                    this.showMessage("You need the car keys to open the car.");
+                }
+            })
+        car.setScale(0.8)
+
+        let alfred = this.add.image(300, 850,'alfred')
+            .setInteractive()
+            .on('pointerover', () => this.showMessage("That's Alfred (you), he's pretty chill."))
+            .on('pointerdown', () => {
+                ding.play()
+                this.showMessage("Please don't poke him, he'll cry (not really but you get the idea).");
+            });
+        alfred.setScale(0.7)
+    }
+}
+
+class Store extends AdventureScene {
+    constructor () {
+        super("store", "Store");
+    }
+    
+    preload() {
+        this.load.image('alfred', "images/alfred.png");
+        this.load.image('store', "images/store.png");
+        this.load.image('arrow', "images/arrow.png");
+        this.load.image('box', "images/box.png");
+        this.load.image('door', "images/door.png");
+        this.load.audio('main', "audio/main_theme.mp3");
+        this.load.audio('ding', "audio/ding_sound.mp3");
+    }
+    onEnter() {
+        let storebg = this.add.image(0,0,'store')
+        storebg.setOrigin(0);
+        storebg.setDepth(0);
+        storebg.setScale(2.85,2);
+
+        let bgtheme = this.sound.add('main')
+        bgtheme.play()
+        let ding = this.sound.add('ding')
+
+        let arrow = this.add.image(1000, 1000, 'arrow')
+        .setInteractive()
+        .on('pointerover', () => {
+            this.showMessage("To Second Street")
+        })
+        .on('pointerdown', () => {
+            ding.play()
+            bgtheme.stop()
+            this.gotoScene('street2')
+        })
+        arrow.setScale(0.4)
+        arrow.angle = 90
+
+        let arrow2 = this.add.image(800, 500, 'arrow')
+        .setInteractive()
+        .on('pointerover', () => {
+            this.showMessage("To Back Alley");
+        })
+        .on('pointerdown', () => {
+            ding.play()
+            bgtheme.stop()
+            this.gotoScene('alley_behind_the_store');
+        })
+        arrow2.setScale(0.4)
+
+        let door = this.add.image(1100, 530, 'door');
+        door.setScale(0.4)
+
+        let alfred = this.add.image(500, 800,'alfred')
+        .setInteractive()
+        .on('pointerover', () => this.showMessage("That's Alfred (you), he's pretty chill."))
+        .on('pointerdown', () => {
+            ding.play()
+            this.showMessage("Please don't poke him, he'll cry (not really but you get the idea).");
+        });
+        alfred.setScale(0.7)
+    
+        // first box settings
+        let box = this.add.image(400, 450, 'box')
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("A box. Open?")
+            })
+            .on('pointerdown', () => {
+                ding.play()
+                this.showMessage("Nothing there. Well, that's upsetting.");
+            })
+        box.setScale(0.5)
+    
+        // second box settings
+        let box2 = this.add.image(1200, 800, 'box')
+        .setInteractive()
+        .on('pointerover', () => {
+            this.showMessage("A box. Open?")
+        })
+        .on('pointerdown', () => {
+            ding.play()
+            this.showMessage("Boxing gloves? Maybe they'll be useful later.");
+            this.gainItem('Boxing gloves');
+        })
+        box2.setScale(0.5)
+    
+        // third box settings
+        let box3 = this.add.image(750, 800, 'box')
+        .setInteractive()
+        .on('pointerover', () => {
+            this.showMessage("A box. Open?")
+        })
+        .on('pointerdown', () => {
+            ding.play()
+            this.showMessage("Nothing there. Well, that's upsetting.");
+        })
+        box3.setScale(0.5)
+    }
+}
+
+// Alley: completed
+class Alley extends AdventureScene {
+    constructor () {
+        super("alley_behind_the_store", "Alley Behind the Store");
+    }
+
+    preload() {
+        this.load.image('alfred', "images/alfred.png");
+        this.load.image('alley', "images/alley_behind_the_store.png");
+        this.load.image('arrow', "images/arrow.png");
+        this.load.image('garr', "images/garr.png");
+        this.load.image('gas_tank', "images/gas_tank.png");
+        this.load.audio('battle', "audio/battle_theme.mp3");
+        this.load.audio('ding', "audio/ding_sound.mp3");
+    }
+
+    onEnter() {
+        // background settings
+        let alleybg = this.add.image(0,0,'alley')
+        alleybg.setOrigin(0);
+        alleybg.setDepth(0);
+        alleybg.setScale(2.85,2);
+
+        //audio settings
+        let bgaudio = this.sound.add('battle')
+        bgaudio.play()
+        let ding = this.sound.add('ding')
+
+        // assets settings
+        let gastank = this.add.image(1000,900,'gas_tank')
+        .setInteractive()
+        .on('pointerover', () => {
+            this.showMessage("Gas. Cars drink it...and the Garr.")
+        })
+        .on('pointerdown', () => {
+            ding.play()
+            this.gainItem('Gas tank');
+            this.tweens.add({
+                targets: gastank,
+                alpha: { from: 1, to: 0 },
+                duration: 500,
+                onComplete: () => gastank.destroy()
+            })
+        })
+        gastank.setScale(0.4)
+
+        let garr = this.add.image(1000, 900, 'garr')
+        .setInteractive()
+        .on('pointerover', () => {
+            this.showMessage("This is a Garr. Green, slimy, likes to drink gas, and smells musty. Ew.")
+        })
+        .on('pointerdown', () => {
+            if (this.hasItem("Boxing gloves")) {
+                ding.play()
+                this.loseItem("Boxing gloves");
+                this.showMessage("You punched it! Somehow that defeats it?");
+                this.tweens.add({
+                    targets: garr,
+                    alpha: { from: 1, to: 0 },
+                    duration: 500,
+                    onComplete: () => garr.destroy()
+                });
+            }
+            else {
+                ding.play()
+                this.showMessage("There's no way you can fight the Garr like that! You need a special item!");
+            }
+            
+        })
+        garr.setScale(0.7)
+
+        let alfred = this.add.image(500, 850,'alfred')
+        .setInteractive()
+        .on('pointerover', () => this.showMessage("That's Alfred (you), he's pretty chill."))
+        .on('pointerdown', () => {
+            ding.play()
+            this.showMessage("Please don't poke him, he'll cry (not really but you get the idea).");
+        });
+        alfred.setScale(0.7)
+
+        let arrow = this.add.image(200, 100, 'arrow')
+        .setInteractive()
+        .on('pointerover', () => {
+            this.showMessage("To Store")
+        })
+        .on('pointerdown', () => {
+            ding.play()
+            bgaudio.stop()
+            this.gotoScene('store')
+        })
+        arrow.setScale(0.4)
+        arrow.angle = 180
     }
 }
 //Intro scene: completed
@@ -222,15 +467,23 @@ class Intro extends Phaser.Scene {
         super('intro')
     }
     create() {
-        this.add.text(this.game.renderer.width / 2.8, this.game.renderer.height / 2, "Are you ready?").setFontSize(70).setFontFamily('Courier New');
-        this.add.text(this.game.renderer.width / 3.5, this.game.renderer.height * 0.6,"Click to begin your adventure!").setFontSize(50).setFontFamily('Courier New');
+
+        this.add.text(this.game.renderer.width / 2.8, this.game.renderer.height / 2, "Are you ready?")
+        .setFontSize(70)
+        .setFontFamily('Courier New');
+        
+        this.add.text(this.game.renderer.width / 3.1, this.game.renderer.height * 0.6,"Click to begin your adventure!")
+        .setFontSize(40)
+        .setFontFamily('Courier New');
+
         this.input.on('pointerdown', () => {
             this.cameras.main.fade(1000, 0,0,0);
-            this.time.delayedCall(1000, () => this.scene.start('street1'));
+            this.time.delayedCall(1000, () => this.scene.start('loading_screen'));
         });
     }
 }
 
+// Loding screen: completed
 class Loading_Screen extends Phaser.Scene{
     constructor() {
         super('loading_screen')
@@ -250,6 +503,7 @@ class Loading_Screen extends Phaser.Scene{
         })
         let txt = this.add.text(this.game.renderer.width / 3, this.game.renderer.height / 2.3,'Loading').setFontSize(100).setFontFamily('Impact');
         txt.setStroke('#000000',16);
+
         this.time.delayedCall(3000, () => {
             this.cameras.main.fadeOut(2000);
         });
@@ -258,15 +512,23 @@ class Loading_Screen extends Phaser.Scene{
     }
 }
 
+// Outro: completed
 class Outro extends Phaser.Scene {
     constructor() {
         super('outro');
     }
     create() {
-        this.add.text(this.game.renderer.width / 2.8, this.game.renderer.height / 2, "Congrats, you escaped!")
+
+        this.add.text(this.game.renderer.width / 3.7, this.game.renderer.height / 2, 
+        "Congrats, you escaped!")
         .setFontSize(70)
         .setFontFamily('Courier New');
-        this.add.text(50, 100, "Click anywhere to restart.").setFontSize(20);
+
+        this.add.text(this.game.renderer.width / 3, this.game.renderer.height * 0.6, 
+        "Click anywhere to start over!")
+        .setFontSize(40)
+        .setFontFamily('Courier New');
+
         this.input.on('pointerdown', () => this.scene.start('intro'));
     }
 }
@@ -279,7 +541,7 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [Intro, Street1, Street2, Street3, Outro],
+    scene: [Intro, Loading_Screen, Street1, Street2, Street3, Store, Alley, Outro],
     title: "Adventure Game",
 });
 
